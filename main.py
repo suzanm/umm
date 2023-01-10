0d90dc299b799290b03c854665766668cbddeb0a
SM - 11TM 708139 - <121039273+suzanm@users.noreply.github.com>
2023-01-10 17:51:27 +0000

    updated

--- main.py
+++ main.py
@@ -1,26 +1,151 @@
- 
-#notactualcode
-
-from tkinter import *
-import random
-import time
-from tkinter import ttk
-from tkinter import messagebox
+import tkinter
 import sqlite3
+import tkinter as tk
 
-def system():
-    root = Tk()
-    root.geometry("1700x800")
-    root.title("Coffee Shop Management")
 
-# Topframe
-topframe = Frame(bg="floral white", width=1600, height=50)
-topframe.pack(side=TOP)
 
-# Leftframe
-leftframe = Frame(width=900, height=700)
-leftframe.pack(side=LEFT)
+# Connect to the database
+conn = sqlite3.connect('coffee_shop.db')
 
-# rightframe
-rightframe = Frame(width=400, height=700)
-rightframe.pack(side=RIGHT)
+# Create a cursor
+cursor = conn.cursor()
+
+# Checks if the products table exists
+cursor.execute('''
+    SELECT name FROM sqlite_master WHERE type='table' AND name='products'
+''')
+
+# If the table does not exist, creates it
+if not cursor.fetchone():
+  cursor.execute('''
+      CREATE TABLE products (
+          id INTEGER PRIMARY KEY,
+          name TEXT NOT NULL,
+          price REAL NOT NULL
+      )
+  ''')
+
+# Inserts some products
+cursor.execute('''
+    INSERT INTO products (name, price) VALUES
+    ('Coffee', 3.50),
+    ('Tea', 2.50),
+    ('Hot Chocolate', 3.00)
+''')
+
+root = tk.Tk()
+
+# Checks if the orders table exists
+cursor.execute('''
+    SELECT name FROM sqlite_master WHERE type='table' AND name='orders'
+''')
+
+#Table with orders
+if not cursor.fetchone():
+    cursor.execute('''
+        CREATE TABLE orders (
+            id INTEGER PRIMARY KEY,
+            product_id INTEGER NOT NULL,
+            quantity INTEGER NOT NULL,
+            total_price REAL NOT NULL,
+            FOREIGN KEY (product_id) REFERENCES products(id)
+        )
+    ''')
+
+# Orders
+cursor.execute('''
+    INSERT INTO orders (product_id, quantity, total_price) VALUES
+    (1, 2, 7.00),
+    (2, 3, 7.50),
+    (3, 1, 3.00)
+''')
+
+  
+class OrderForm(tk.Frame):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Frame.__init__(self, parent, *args, **kwargs)
+
+        # Create a Listbox for selecting the product
+        self.product_list = tk.Listbox(self)
+
+        # Populate the Listbox with the available products
+        cursor.execute('''
+            SELECT name FROM products  
+        ''')
+        for row in cursor:
+            self.product_list.insert(tk.END, row[0])
+
+        # Create a widget for entering the quantity
+        self.quantity_label = tk.Label(self, text="Quantity")
+        self.quantity_entry = tk.Entry(self)
+
+        # Create a button to submit the order
+        self.submit_button = tk.Button(self, text="Submit", command=self.place_order)
+
+        # Lay out the widgets in a grid
+        self.product_list.grid(row=0, column=0)
+        self.quantity_label.grid(row=1, column=0)
+        self.quantity_entry.grid(row=1, column=1)
+        self.submit_button.grid(row=2, column=0, columnspan=2)
+
+    def place_order(self):
+      # Get the selected product and quantity from the form
+      product = self.product_list.get(self.product_list.curselection())
+      quantity = int(self.quantity_entry.get())
+  
+      # Retrive product id
+      cursor.execute('''
+          SELECT id, price FROM products WHERE name=?
+      ''', (product,))
+      product_id, price = cursor.fetchone()
+
+      # Total Price
+      total_price = quantity * price
+
+      # Add order to database
+      cursor.execute('''
+        INSERT INTO orders (product_id, quantity, total_price) VALUES (?, ?, ?)
+      ''', (product_id, quantity, total_price))
+      conn.commit()
+
+
+if not hasattr(root, 'form'): root.form = OrderForm(root)
+form = OrderForm(root)
+form.pack()
+root.mainloop()
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# Commit changes
+conn.commit()
+
+# Select all products
+cursor.execute('''
+    SELECT * FROM products
+''')
+
+# Print the products
+print('PRODUCTS:')
+print('----------')
+products = cursor.fetchall()
+for row in products:
+    print(f'ID: {row[0]} | Name: {row[1]} | Price: ${row[2]:.2f}')
+
+# Create a new cursor for the orders
+cursor = conn.cursor()
+
+# Select all orders
+cursor.execute('''
+    SELECT * FROM orders
+''')
+
+# Print the orders
+print('\nORDERS:')
+print('----------')
+orders = cursor.fetchall()
+for row in orders:
+    print(f'ID: {row[0]} | Product ID: {row[1]} | Quantity: {row[2]} | Total Price: ${row[3]:.2f}')
+
+  
+# Close the connection
+conn.close()
